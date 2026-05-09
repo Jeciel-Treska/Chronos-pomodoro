1d54273322afa386d440f58c3ee31937ef40a6ed
Jeciel Treska <jecieltreska@gmail.com>
2026-05-08 21:42:02 -0300

    Aula25 - Criado componente de CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ContainerCompo } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -14,6 +15,10 @@ export function App() {
 
       <ContainerCompo>
         <Menu />
+      </ContainerCompo>
+
+      <ContainerCompo>
+        <CountDown />
       </ContainerCompo>
     </>
   );
