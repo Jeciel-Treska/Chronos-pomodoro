00d3623b97479ea75948ece305699fe5b8caf40a
Jeciel Treska <jecieltreska@gmail.com>
2026-05-08 21:50:42 -0300

    Aula26 - Criado formulario da pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,28 @@ export function App() {
       <ContainerCompo>
         <CountDown />
       </ContainerCompo>
+
+      <ContainerCompo>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="input">Task</label>
+            <input id="input" type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </ContainerCompo>
     </>
   );
 }
